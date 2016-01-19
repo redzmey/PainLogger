6c7f2b31ca75e7c81a2d03acf169a663b062d96f
Andrey <red.zmey@gmail.com>
2016-01-20 00:23:05 +0200

    await bug resolved

---
+++ PainLogger.Unittest/MedicineUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using PainLogger.Model.Repositories;
+
+namespace PainLogger.Unittest
+{
+
+    public class MedicineUnitTest
+    {
+        [Fact]
+        public void RepositoryShouldReturnSomething()
+        {
+            var repo = new MedicineRepository();
+
+            Assert.NotNull(repo.GetAll());
+        }
+    }
+}
