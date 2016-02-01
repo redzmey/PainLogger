fe7bc6c0f90c87859e190d78c3e264283043ad30
redzmey <red.zmey@live.com>
2016-02-02 00:45:30 +0200

    ---

--- PainLogger.Model/Validation/MedicineValidator.cs
+++ PainLogger.Model/Validation/MedicineValidator.cs
@@ -1,14 +1,14 @@
-﻿using FluentValidation;
-using PainLogger.Model.Models;
+﻿//using FluentValidation;
+//using PainLogger.Model.Models;
 
-namespace PainLogger.Model.Validation
-{
-    public class MedicineValidator : AbstractValidator<Medicine>
-    {
-        public MedicineValidator()
-        {
-            RuleFor(x => x.Name).NotNull();
-            RuleFor(x => x.Dosage).GreaterThanOrEqualTo(0).NotNull();
-        }
-    }
-}
+//namespace PainLogger.Model.Validation
+//{
+//    public class MedicineValidator : AbstractValidator<Medicine>
+//    {
+//        public MedicineValidator()
+//        {
+//            RuleFor(x => x.Name).NotNull();
+//            RuleFor(x => x.Dosage).GreaterThanOrEqualTo(0).NotNull();
+//        }
+//    }
+//}
